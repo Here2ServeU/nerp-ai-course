b029ad26418371e9e5574549e095e0700b02fbf8
Emmanuel Naweji <revnaweji@gmail.com>
2026-03-11 04:41:38 -0500

    Create dataset_intro.py with sample data
    Added a dataset with CPU and memory usage statistics.

--- course/day02_ai_basics/dataset_intro.py
+++ course/day02_ai_basics/dataset_intro.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+data = {
+    "cpu_usage": [45, 50, 90, 95],
+    "memory_usage": [60, 65, 80, 85],
+    "system_status": ["normal", "normal", "warning", "failure"]
+}
+
+df = pd.DataFrame(data)
+
+print("Dataset Preview:")
+print(df)
